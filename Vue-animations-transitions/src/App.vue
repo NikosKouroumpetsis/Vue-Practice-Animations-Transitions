794ad82076bcf75f15c649b8961a3997cb0d40ff
Nikolaos Kouroumpetsis <nikolaos.kouroumpetsis@gmail.com>
2022-11-09 12:04:10 +0200

    animations-group-animations

--- Vue-animations-transitions/src/App.vue
+++ Vue-animations-transitions/src/App.vue
@@ -1,11 +1,154 @@
 <template>
-  <div></div>
+  <button type="button" @click="flag = !flag">Toggle</button>
+
+  <!-- <transition name="fade" mode="out-in">
+    <h2 v-if="flag" key="main">Hello world!!</h2>
+    <h2 v-else key="secondary">v-else Hello</h2>
+  </transition> -->
+
+  <!-- <transition name="zoom" type="animation" appear>
+    <h2 v-if="flag">hello</h2>
+  </transition> -->
+  <!-- 
+  <transition
+    @before-enter="beforeEnter"
+    @enter="enter"
+    @after-enter="afterEnter"
+    @before-leave="beforeLeave"
+    @leave="leave"
+    @after-leave="afterLeave"
+    :css="true"
+    name="fade"
+  >
+    <h2 v-if="flag">Hey JS Animations</h2>
+  </transition> -->
+
+  <button @click="addItem">Add</button>
+  <ul>
+    <transition-group name="zoom">
+      <li
+        v-for="(number, index) in numbers"
+        :key="number"
+        @click="removeItem(index)"
+      >
+        {{ number }}
+      </li>
+    </transition-group>
+  </ul>
 </template>
 
 <script>
 export default {
   name: "App",
+  data() {
+    return {
+      flag: true,
+      numbers: [1, 2, 3, 4, 5],
+    };
+  },
+  methods: {
+    addItem() {
+      const num = Math.floor(Math.random() * 100 + 1);
+      const index = Math.floor(Math.random() * this.numbers.length);
+      this.numbers.splice(index, 0, num);
+    },
+    removeItem(index) {
+      this.numbers.splice(index, 1);
+    },
+    beforeEnter(el) {
+      console.log("before-enter event fn", el);
+    },
+    enter(el) {
+      console.log("enter event fn", el);
+
+      // const animation = el.animate([{ transform: "scale3d(0,0,0)" }, {}], {
+      //   duration: 1000,
+      // });
+      // animation.onfinish = () => {
+      //   done();
+      // };
+    },
+    afterEnter(el) {
+      console.log("afer-enter event fn", el);
+    },
+    beforeLeave(el) {
+      console.log("before-leave event fn", el);
+    },
+    leave(el) {
+      // const animation = el.animate([{}, { transform: "scale3d(0,0,0)" }], {
+      //   duration: 1000,
+      // });
+      // animation.onfinish = () => {
+      //   done();
+      // };
+
+      console.log("leave event fn", el);
+    },
+    afterLeave(el) {
+      console.log("after-leave event fn", el);
+    },
+  },
 };
 </script>
 
-<style></style>
+<style>
+li {
+  font-size: 22px;
+  cursor: pointer;
+}
+
+h2 {
+  width: 400px;
+  padding: 20px;
+  margin: 20px;
+}
+
+.fade-enter-from {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: all 1s linear;
+}
+
+.fade-leave-to {
+  transition: all 1s linear;
+  opacity: 0;
+}
+
+.zoom-enter-active {
+  animation: zoom-in 1s linear forwards;
+  transition: all 2s linear;
+}
+
+.zoom-leave-active {
+  animation: zoom-out 1s linear forwards;
+  transition: all 2s linear;
+}
+
+.zoom-enter-from {
+  opacity: 0;
+}
+
+.zoom-leave-to {
+  opacity: 0;
+}
+
+@keyframes zoom-in {
+  from {
+    transform: scale(0, 0);
+  }
+  to {
+    transform: scale(1, 1);
+  }
+}
+
+@keyframes zoom-out {
+  from {
+    transform: scale(1, 1);
+  }
+  to {
+    transform: scale(0, 0);
+  }
+}
+</style>
